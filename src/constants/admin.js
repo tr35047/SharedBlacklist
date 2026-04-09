a62f305f6789492084f8aec6acbf047f47103b20
tr35047 <tr35047@outlook.com>
2026-04-09 16:13:27 +0800

    Revert "pub更新"
    This reverts commit ba8d48a7111fa431e3d0444dec7687bb37dcf713.

--- src/constants/admin.js
+++ src/constants/admin.js
@@ -1,5 +1,5 @@
 // 管理员公钥 — 首次部署时生成并硬编码
-export const ADMIN_PUB_KEY = 'NcT-wk1-RDeFdmZuKXaejmLKY3h5sW_o9qF8xCs6mfI.SU_oY2y3li38aRtUPW3ZM1n85NlikHTurpItSdH6Vuw'
+export const ADMIN_PUB_KEY = 'HWYIpmPpqSVHT6wi06LEynydQtBivOfcKI7eL2rSyCk.-ACYpEeOLxoauSdhTPntUOeTygTRDmqsAk0iFcWVrWg'
 
 // 管理员别名（Gun 用户系统）
 export const ADMIN_ALIAS = 'blacklist-admin'
